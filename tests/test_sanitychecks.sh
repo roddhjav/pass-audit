2a24954e4c3850b2b195274a52079891d71cbc23
Alexandre Pujol <alexandre@pujol.io>
2019-02-01 18:59:46 +0000

    Remove test install

--- tests/test_sanitychecks.sh
+++ tests/test_sanitychecks.sh
@@ -13,14 +13,4 @@ test_expect_success 'Testing help message' '
     _pass audit --version | grep "pass audit"
     '
 
-if test_have_prereq CI; then
-    export PASSWORD_STORE_ENABLE_EXTENSIONS=''
-    export PASSWORD_STORE_EXTENSIONS_DIR=''
-    test_expect_success 'Testing extension installation' '
-        make --directory=$EXT_HOME install &&
-        _pass audit --version | grep "pass audit 1.0" &&
-        make --directory=$EXT_HOME uninstall
-        '
-fi
-
 test_done
